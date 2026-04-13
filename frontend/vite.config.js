0a3f0627df54923bc3af6fcc9cd56d39a476bc33
Shravni1505 <shravni.garde@cumminscollege.in>
2026-04-13 23:00:06 +0530

    Added CI pipeline

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "localhost",
+    host: "0.0.0.0",
     port: 5173,
     strictPort: false,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "http://backend:4000",
         changeOrigin: true,
         secure: false,
         ws: true,
