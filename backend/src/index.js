3fd38610141053f3bcdc2e0fe9a8bb35e5e505b6
Shravni1505 <shravni.garde@cumminscollege.in>
2026-04-14 13:27:18 +0530

    Fix morgan logger import path

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json({ limit: "10mb" }));
-const morganLogger = require("../utils/morganLogger");
+const morganLogger = require("./utils/morganLogger");
 //console.log("Morgan Logger:", morganLogger);
 app.use(morganLogger);
 //app.use(morgan("dev")); // optional: keep console logs too
