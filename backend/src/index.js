e12a65a3c0ae36bb0211a0cd55d29fd9d2d0ee29
Shravni1505 <shravni.garde@cumminscollege.in>
2026-04-14 10:58:58 +0530

    Fixed CORS and typo

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,10 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: "*",
+  origin: [
+    "http://localhost:5173",
+    "https://samruddhi-frontend.onrender.com"
+  ],
   credentials: true
 }));
 app.use(express.json({ limit: "10mb" }));
