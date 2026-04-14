1d6a89479f0e504b80fcc7758ec622b62767d51c
Shravni1505 <shravni.garde@cumminscollege.in>
2026-04-14 10:34:57 +0530

    Fixed CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: "*",
   credentials: true
 }));
 app.use(express.json({ limit: "10mb" }));
